b82e6fb3f675ae0cec1b4f8d6241c9c84caa6401
malviyaaayushi <malviyaaayushi@gmail.com>
2016-01-27 11:57:14 +0530

    Backpropagation running now. Add updateWeights functions to all classes.

--- neuron.py
+++ neuron.py
@@ -42,6 +42,11 @@ class NeuralLayer:
 		for j in range(self._numNeurons):
 			opMatrix[j] = self._neurons[j].compute(ip)
 		return opMatrix
+	
+	def updateWeights(self, newNeuronWeightsInLayer):
+		self._neuronWeightsInLayer = newNeuronWeightsInLayer
+		for i in range(self._numNeurons):
+			self._neurons[i].updateWeights(self._neuronWeightsInLayer[i])
 
 class NeuralNetwork:
 	def __init__(self, numLayers, numNeuronsPerLayer, weightMatrix=[], activationFunctions=[]):
@@ -69,7 +74,7 @@ class NeuralNetwork:
 								print("Error: Insufficient weights provided for neuron "+str(j+1)+" in Layer "+str(i+1))
 								sys.exit(0)
 			self._weightMatrix = weightMatrix
-		self._weightMatrix = [[np.matrix(self._weightMatrix[i][j]) for j in range(numNeuronsPerLayer[i+1])] for i in range(self._numLayers)]
+		self._weightMatrix = [[np.matrix(self._weightMatrix[i][j]) for j in range(self._numNeuronsPerLayer[i+1])] for i in range(self._numLayers)]
 		if activationFunctions==[]:
 			self._activationFunctions = [Threshold(1) for i in range(self._numLayers)]
 		else:
@@ -81,7 +86,7 @@ class NeuralNetwork:
 		self._layers = [NeuralLayer(numNeuronsPerLayer[i+1], self._weightMatrix[i], self._activationFunctions[i]) for i in range(self._numLayers)]
 
 	def forwardPropagation(self, ip):
-		aMatrix = np.matrix(ip)
+		aMatrix = ip
 		activationValues = []
 		for i in range(self._numLayers):
 			aMatrix = np.matrix(self._layers[i].compute(aMatrix))
@@ -102,10 +107,7 @@ class NeuralNetwork:
 			z[i] = x.item(i) * y.item(i)
 		return np.matrix(z)
 
-	def backPropagation(self, ip, expectedOutput):
-		ip = np.matrix(ip)
-		expectedOutput = np.matrix(expectedOutput)
-		activationValues = self.forwardPropagation(ip)
+	def backPropagationUtil(self, expectedOutput, activationValues):
 		delta = [None]*self._numLayers
 		activationFunctionDerivative = [None]*self._numLayers
 		for i in range(self._numLayers):
@@ -114,7 +116,6 @@ class NeuralNetwork:
 		# Set delta for output layer
 		delta[self._numLayers-1] = self.dot((activationValues[self._numLayers-1] - expectedOutput), activationFunctionDerivative[self._numLayers-1])
 		
-		delW = [None]*self._numLayers
 		# set delta for all layers
 		for l in range(self._numLayers-2, -1, -1):
 			aggr = [0.0]*self._numNeuronsPerLayer[l+1]
@@ -124,6 +125,25 @@ class NeuralNetwork:
 			aggr = np.matrix(aggr)
 			delta[l] = self.dot(aggr, activationFunctionDerivative[l])
 		
+		return delta
+
+	def error(self, delta):
+		e = 0.0
+		for i in range(self._numNeuronsPerLayer[self._numLayers]):
+			e += pow(delta[self._numLayers-1].item(i), 2)
+		return e/2
+
+	def updateWeights(self, newWeights):
+		self._weightMatrix = newWeights
+		for i in range(self._numLayers):
+			self._layers[i].updateWeights(self._weightMatrix[i])
+
+		#print("Updated WeightMatrix:")
+		#print(self._weightMatrix)
+			
+
+	def backPropagationUpdateWeight(self, ip, delta, activationValues):
+		delW = [None]*self._numLayers
 		for l in range(self._numLayers):
 			delW[l] = [None]*self._numNeuronsPerLayer[l]
 			if l==0:
@@ -132,9 +152,16 @@ class NeuralNetwork:
 				y = activationValues[l-1]
 			delW[l] = (delta[l].transpose() * y)
 		
-		return delW
+		newWeightMatrix = [[None for j in range(self._numNeuronsPerLayer[i+1])] for i in range(self._numLayers)]
 
-	def backPropagationUpdateWeight(self, batchInputs, expectedBatchOutputs, alpha):
+		for j in range(self._numLayers):
+			for k in range(self._numNeuronsPerLayer[j+1]):
+				newWeightMatrix[j][k] = self._weightMatrix[j][k] - delW[j][k]
+
+		self.updateWeights(newWeightMatrix)
+	
+	
+	def backPropagation(self, batchInputs, expectedBatchOutputs, alpha, e):
 		if len(batchInputs)!=len(expectedBatchOutputs):
 			print("Error: Invalid input provided. Size of batchInputs and expectedBatchOutputs differ.")
 			sys.exit(0)
@@ -142,14 +169,26 @@ class NeuralNetwork:
 		for i in range(ipCount):
 			ip = np.matrix(batchInputs[i])
 			expectedOutput = np.matrix(expectedBatchOutputs[i])
-			delW = self.backPropagation(ip, expectedOutput)
-			for j in range(self._numLayers):
-				for k in range(self._numNeuronsPerLayer[j+1]):
-					self._weightMatrix[j][k] = self._weightMatrix[j][k] + delW[j][k]
+			activationValues = self.forwardPropagation(ip)
+			
+			delta = self.backPropagationUtil(expectedOutput, activationValues)
 
-		print("Updated WeightMatrix:")
-		print(self._weightMatrix)
+			er = self.error(delta)
+			while er >= e:
 
+				self.backPropagationUpdateWeight(ip, delta, activationValues)
+				
+				activationValues = self.forwardPropagation(ip)
+
+				delta = self.backPropagationUtil(expectedOutput, activationValues)
+				er = self.error(delta)	
+		
+			print("Final layer output: "+str(activationValues[self._numLayers-1]))
+			print("Backpropagation performed successfully. Final error: "+str(er))	
+
+
+		
+		
 
 
 
